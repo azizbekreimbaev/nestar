6336acd156748262be471e342858d1e85180e930
Azizbek <azizbekreimbaev1003@gmail.com>
2026-08-26 23:48:22 +0900

    feat: developed createProperty graphql api

--- apps/nestar-api/src/components/property/property.service.ts
+++ apps/nestar-api/src/components/property/property.service.ts
@@ -1,8 +1,28 @@
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
-import { Injectable } from '@nestjs/common';
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import { PropertyInput } from '../../libs/dto/property/property.input';
+import { Property } from '../../libs/dto/property/property';
+import { Message } from '../../libs/enums/common.enum';
 
 @Injectable()
 export class PropertyService {
-    constructor(@InjectModel("Property") private readonly propertyModel: Model<void>) { }
+    constructor(@InjectModel("Property") private readonly propertyModel: Model<Property>) { }
+
+    public async createProperty(input: PropertyInput): Promise<Property> {
+        try {
+            const result = await this.propertyModel.create(input)
+
+            //increase memberProperty
+
+            return result
+
+
+        } catch (err) {
+            console.log("ERROR, on createProperty", err)
+            throw new InternalServerErrorException(Message.CREATE_FAILED)
+        }
+
+    }
+
 }
