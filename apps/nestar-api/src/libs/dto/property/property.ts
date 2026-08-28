a3b6c3acec7aa703d4a057a79de88c1f81e1f33e
Azizbek <azizbekreimbaev1003@gmail.com>
2026-08-28 12:30:47 +0900

    feat: developed getProperty graphql api

--- apps/nestar-api/src/libs/dto/property/property.ts
+++ apps/nestar-api/src/libs/dto/property/property.ts
@@ -1,6 +1,7 @@
 import { Field, Int, ObjectType } from "@nestjs/graphql";
 import type { ObjectId } from 'mongoose'
 import { PropertyLocation, PropertyStatus, PropertyType } from "../../enums/property.enum";
+import { Member } from "../member/member";
 
 
 @ObjectType()
@@ -91,5 +92,9 @@ export class Property {
     @Field(() => Date,)
     updatedAt!: Date;
 
+    //aggregate
+
+    @Field(() => Member, { nullable: true })
+    memberData?: Member
 
 }
