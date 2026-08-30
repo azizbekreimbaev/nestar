5973d397027edaa43da70683dbf8a665552ef950
Azizbek <azizbekreimbaev1003@gmail.com>
2026-08-30 14:03:38 +0900

    feat: integrated board articel dto and configured board article module

--- apps/nestar-api/src/components/components.module.ts
+++ apps/nestar-api/src/components/components.module.ts
@@ -18,6 +18,6 @@ import { BoardArticleModule } from './board-article/board-article.module';
     ViewModule,
     FollowModule,
     BoardArticleModule,
-  ]
+  ],
 })
 export class ComponentsModule { }
