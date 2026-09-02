b8a543a339d7f9ab335f7ed097a32fc359f6bd95
Azizbek <azizbekreimbaev1003@gmail.com>
2026-09-03 06:29:52 +0900

    feat: developed DTO and resolver/service of follow module

---
+++ apps/nestar-api/src/components/follow/follow.resolver.ts
@@ -0,0 +1,10 @@
+import { Resolver } from '@nestjs/graphql';
+import { FollowService } from './follow.service';
+
+@Resolver()
+export class FollowResolver {
+    constructor(private readonly followService: FollowService) { }
+
+    
+
+}
